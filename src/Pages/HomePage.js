85f9299536df820d310e84e0e52af08bcceab5b0
unknown <charayaamit@gmail.com>
2021-10-14 13:36:37 +0530

    first changes

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,9 @@
+import Header from 'ui/Header';
+function HomeScreen(){
+return (
+<div>
+<Header/>
+</div>
+)
+}
+export default HomeScreen;
---
+++ src/ui/Header.js
@@ -0,0 +1,12 @@
+import logo from 'assets/logo-full.png';
+function Header(){
+return(
+<div>
+<img className="header__logo"
+src={logo}
+alt="Netflix Logo"
+</div>
+);
+
+}
+export default Header;
